3ad4ded5d063094397ffc23dcc4fd462ba95135c
NELSON DAMICO JUNIOR <133600275+neodamico@users.noreply.github.com>
2024-01-03 18:21:20 -0300

    Add files via upload

---
+++ calculadora_potencia.rb
@@ -0,0 +1,20 @@
+numeros = []
+
+i = 1
+
+1..3.times do
+
+print "Digite o #{i}º número: "
+
+numeros.push gets.chomp.to_i
+
+i += 1
+
+end
+
+numeros.map! do |numeros|
+    numeros**3
+end
+
+print numeros
+
